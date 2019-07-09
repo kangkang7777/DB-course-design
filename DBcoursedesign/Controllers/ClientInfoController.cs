74f8811c9c069aac01dd014cb27f992222a1c144
EgbertHistorianPokling <39192657+EgbertHistorianPokling@users.noreply.github.com>
2019-07-09 09:34:28 +0800

    Add files via upload

---
+++ DBcoursedesign/Controllers/ClientInfoController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBcoursedesign.Controllers
+{
+    public class ClientInfoController : Controller
+    {
+        // GET: ClientInfo
+        public ActionResult PersonInformation()
+        {
+            return View();
+        }
+
+        public ActionResult SaveInfo(InfoContact et)
+        {
+            if (ModelState.IsValid)
+            {
+                ClientInfoController empbal = new ClientInfoController();
+                empbal.SaveInfo(et);
+                return RedirectToAction("PersonInformation");
+            }
+            else
+            {
+                return View("PersonInformation");
+            }
+        }
+        public ActionResult SaveInfo()
+        {
+            InfoContact ev = new InfoContact();
+            ev.ClientName = Request.Form["ClientName"];
+            ev.sex = Request.Form["sex"];
+            ev.age = Request.Form["age"];
+            ev.IDNum = Request.Form["IDNum"];
+            ev.ClientTel = Request.Form["ClientTel"];
+            ev.ClientAddress = Request.Form["ClientAddress"];
+            return RedirectToAction("PersonInformation");
+        }
+    }
+}
---
+++ DBcoursedesign/Controllers/SearchInfoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DBcoursedesign.Controllers
+{
+    public class SearchInfoController : Controller
+    {
+        private static List<Contact> contacts = new List<Contact>
+    {
+    };
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Find(string clientLocation = "", int distance=999999 )
+        {
+            var result = from contact in contacts
+                         where (  string.IsNullOrEmpty(clientLocation) || contact.ClientLocation.ToLower().Contains( clientLocation.ToLower() ))
+                             && ( distance == 999999 || contact.Distance<distance  )
+                         orderby contact.Id
+                         select contact;
+            return View("ContactListPartial", result.ToArray());
+        }
+
+        public ActionResult Find2(/**/)
+        {
+            /*var result = from contact in contacts
+                         where (string.IsNullOrEmpty(clientLocation) || contact.ClientLocation.ToLower().Contains(clientLocation.ToLower()))
+                             && (distance == 999999 || contact.Distance < distance)
+                         orderby contact.Id
+                         select contact;*/
+            return View("ContactListPartial2"/*, result.ToArray()*/);
+        }
+
+        [HttpGet]
+        public ActionResult Update(string contactId)
+        {
+            Contact contact = contacts.First(c => c.Id == contactId);
+            return View("ContactPartial", contact);
+        }
+
+        [HttpPost]
+        public string Update(Contact contact)
+        {
+            contacts.Remove(contacts.First(c => c.Id == contact.Id));
+            contacts.Add(contact);
+            return "OK";
+        }
+
+        [HttpGet]
+        public ActionResult Update2(string contactId)
+        {
+            Contact contact = contacts.First(c => c.Id == contactId);
+            return View("ContactPartial2", contact);
+        }
+
+        [HttpPost]
+        public string Update2(Contact contact)
+        {
+            contacts.Remove(contacts.First(c => c.Id == contact.Id));
+            contacts.Add(contact);
+            return "OK";
+        }
+    }
+}
