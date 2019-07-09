fc370e7434b398d1275fc63afe09906e498fbe30
EgbertHistorianPokling <39192657+EgbertHistorianPokling@users.noreply.github.com>
2019-07-09 11:03:36 +0800

    Add files via upload

---
+++ DBcoursedesign/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Contact
+{
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string ClientLocation { get; set; }
+    [Required]
+    public int Distance { get; set; }
+    [Required]
+    public bool HomePick { get; set; }
+    [Required]
+    public string DistributionCentreName { get; set; }
+    [Required]
+    public string DistributionCentreAddress { get; set; }
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    public string DistributionCentreTel { get; set; }
+    [Required]
+    public string Departure { get; set; }
+    [Required]
+    public string Destination { get; set; }
+    [Required]
+    public string CargoType { get; set; }
+    [Required]
+    public int Weight { get; set; }
+    [Required]
+    public bool Fragile { get; set; }
+}
---
+++ DBcoursedesign/Models/InfoContact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class InfoContact
+{
+    [Required]
+    public string Id { get; set; }
+    [Required]
+    public string ClientName { get; set; }
+    [Required]
+    public string sex { get; set; }
+    [Required]
+    public string age { get; set; }
+    [Required]
+    public string IDNum { get; set; }
+    [Required]
+    [DataType(DataType.PhoneNumber)]
+    public string ClientTel { get; set; }
+    [Required]
+    public string ClientAddress { get; set; }
+}
